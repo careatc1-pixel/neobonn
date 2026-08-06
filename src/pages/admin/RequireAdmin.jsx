473700bf58043cbb4e0dc373ff18e2c2d74a3000
careatc1-pixel <care.atc1@gmail.com>
2026-08-06 10:25:00 +0530

    fix structure

--- src/pages/admin/RequireAdmin.jsx
+++
@@ -1,8 +0,0 @@
-import { Navigate } from "react-router-dom";
-import { ADMIN_LOGIN_PATH } from "../../App";
-
-export default function RequireAdmin({ children }) {
-  const isAdmin = sessionStorage.getItem("neobonn_admin") === "true";
-  if (!isAdmin) return <Navigate to={ADMIN_LOGIN_PATH} replace />;
-  return children;
-}
